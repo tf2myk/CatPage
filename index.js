8f9193afa4794348371c5ef164b0b4d2ce3d0410
tf2myk <tf2myk@gmail.com>
2023-08-20 16:44:10 -0700

    Mainn page setup, backend for uploading, need to add front end implementation

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 const searchcats = require('./searchcats.js')
-const express = require('express');
+const express = require('express');;
 const app = express();
 const port = 3000;
+
+
+/// Image Uploading
+
+const multer = require('multer');
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+
+
+// Google Cloud Storage configuration
+const storageClient = new Storage({
+  keyFilename: 'keys\catkey.json', // Replace with your JSON key file path
+});
+
+const bucketName = 'cata_test_1';
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+  try {
+    const file = req.file;
+
+    if (!file) {
+      res.status(400).json({ error: 'No image file provided' });
+      return;
+    }
+
+    const blob = storageClient.bucket(bucketName).file(file.originalname);
+    const blobStream = blob.createWriteStream();
+
+    blobStream.on('error', (err) => {
+      res.status(500).json({ error: 'Error uploading image' });
+    });
+
+    blobStream.on('finish', () => {
+      const imageUrl = `https://storage.googleapis.com/${bucketName}/${file.originalname}`;
+      res.json({ imageUrl });
+    });
+
+    blobStream.end(file.buffer);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+///
 
 
 // Define a route to serve the HTML page
